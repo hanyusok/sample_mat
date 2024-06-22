4da5c0ac074f7f11799ec7ec403b81b58f905092
hanyusok <hanyusok@gmail.com>
2024-06-22 18:04:49 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool useMaterial3 = true;
+  ThemeMode themeMode = ThemeMode.system;
+  ColorSeed colorSelected = ColorSeed.baseColor;
+  ColorImageProvider imageSelected = ColorImageProvider.leaves;
+  ColorScheme? imageColorScheme = const ColorScheme.light();
+  ColorSelectionMethod colorSelectionMethod = ColorSelectionMethod.colorSeed;
+
+  bool get useLightMode => switch (themeMode) {
+        ThemeMode.system =>
+          View.of(context).platformDispatcher.platformBrightness ==
+              Brightness.light,
+        ThemeMode.light => true,
+        ThemeMode.dark => false
+      };
+
+  void handleBrightnessChange(bool useLightMode) {
+    setState(() {
+      themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
+  void handleColorSelect(int value) {
+    setState(() {
+      colorSelectionMethod = ColorSelectionMethod.colorSeed;
+      colorSelected = ColorSeed.values[value];
+    });
+  }
+
+  void handleImageSelect(int value) {
+    final String url = ColorImageProvider.values[value].url;
+    ColorScheme.fromImageProvider(provider: NetworkImage(url))
+        .then((newScheme) {
+      setState(() {
+        colorSelectionMethod = ColorSelectionMethod.image;
+        imageSelected = ColorImageProvider.values[value];
+        imageColorScheme = newScheme;
+      });
+    });
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material 3',
+      themeMode: themeMode,
+      theme: ThemeData(
+          colorSchemeSeed:
+              colorSelectionMethod == ColorSelectionMethod.colorSeed
+                  ? colorSelected.color
+                  : null,
+          colorScheme: colorSelectionMethod == ColorSelectionMethod.image
+              ? imageColorScheme
+              : null,
+          useMaterial3: useMaterial3,
+          brightness: Brightness.light),
+      darkTheme: ThemeData(
+          colorSchemeSeed:
+              colorSelectionMethod == ColorSelectionMethod.colorSeed
+                  ? colorSelected.color
+                  : imageColorScheme!.primary,
+          useMaterial3: useMaterial3,
+          brightness: Brightness.dark),
+      home: Home(
+        useLightMode: useLightMode,
+        useMaterial3: useMaterial3,
+        colorSelected: colorSelected,
+        imageSelected: imageSelected,
+        handleBrightnessChange: handleBrightnessChange,
+        handleColorSelect: handleColorSelect,
+        handleImageSelect: handleImageSelect,
+        colorSelectionMethod: colorSelectionMethod,
+      ),
+    );
+  }
+}
